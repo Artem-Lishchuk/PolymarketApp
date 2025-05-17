2b247d0124c3d6d695c1416b7471734bad06604c
Artem Lishchuk <lishchuk.artem@gmail.com>
2025-05-18 00:13:46 +0300

    Second Commit

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
-#Testing
-print("Hello, World!")
+# Import the necessary module
+from dotenv import load_dotenv
+import os
+
+def configure():
+    load_dotenv()
+
+
+def main():
+    configure()
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    DATABASE_URL = os.getenv('DATABASE_URL')
+
+    # Example usage
+    print(f'SECRET_KEY: {SECRET_KEY}')
+    print(f'DATABASE_URL: {DATABASE_URL}')
